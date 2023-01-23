d135ef58059f49050d69c4d7bb1ba74fff97bf14
ha <typ0@naver.com>
2023-01-23 20:15:22 +0900

    feat: keydown view

--- packages/sdk/src/models/PlayLog.ts
+++ packages/sdk/src/models/PlayLog.ts
@@ -3,12 +3,14 @@ import { Event } from "./Event";
 import { Validity } from "./Validity";
 import { Running } from "./Running";
 import { Action } from "./Action";
+import { Keydown } from "./Keydown";
 
 export interface PlayLog {
   events: Event[];
   index: number;
   validity_view: Record<string, Validity>;
   running_view: Record<string, Running>;
+  keydown_view: Record<string, Keydown>;
   buff_view: Stat;
   damage: number;
   damages: [string, number][];
